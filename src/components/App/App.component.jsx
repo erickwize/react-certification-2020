e1957928db3ddcf7a3f55247bc4236a31f97f856
candrepa1 <candrepa1@gmail.com>
2021-03-18 14:06:28 -0500

    mc1-doneV1

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../Header/Header.component';
-// import Cards from '../Cards/Cards.component';
 import CardContainer from '../CardContainer/CardContainer.component';
 
 const Main = styled.div`
