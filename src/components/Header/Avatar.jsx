34b1a2c85ac52a827fff418a281bb7b08de40552
Julieta <juliiy98@gmail.com>
2021-03-18 19:21:19 -0700

    added avatar styled component

---
+++ src/components/Header/Avatar.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Avatar = styled.img`
+  background-image: url('https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg');
+  width: 50px;
+  height: 50px;
+  margin-left: 15%;
+  padding: 1%;
+  background-size: cover;
+  background-position: top center;
+  border-radius: 50%;
+`;
