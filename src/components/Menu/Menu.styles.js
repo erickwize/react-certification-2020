feb4df62afb13c5e6972c2874edd4c06052e2a77
AureAle <aperez33@ucol.mx>
2021-03-19 00:14:05 -0600

    Add Menu component

---
+++ src/components/Menu/Menu.styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { MenuOutline } from '@styled-icons/evaicons-outline/MenuOutline';
+
+export const MenuButton = styled(MenuOutline)`
+  color: #fff;
+`;
+
+export const MenuContainer = styled.svg`
+  width: 24px;
+  height: 24px;
+  margin-right: 16px;
+`;
