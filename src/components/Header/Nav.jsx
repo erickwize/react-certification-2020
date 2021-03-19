ebdd618a2648af2a6407fe5a8554a2078f6780b7
Julieta <juliiy98@gmail.com>
2021-03-18 19:21:52 -0700

    add nav styled component

---
+++ src/components/Header/Nav.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Nav = styled.div`
+  background-color: #fff;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.0975);
+`;
+
+export const NavHeader = styled.div`
+  max-width: 1310px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin: 5px auto;
+`;
+
+export const NavLeft = styled.div`
+  width: 20%;
+  text-align: left;
+`;
+
+export const NavCenter = styled.div`
+  width: 58%;
+  text-align: center;
+`;
+
+export const NavRight = styled.div`
+  width: 12%;
+  text-align: right;
+`;
