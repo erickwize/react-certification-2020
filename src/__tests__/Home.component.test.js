e0bc7e8eac095a191cf8df12b326ebaacc5ab7f6
AureAle <aperez33@ucol.mx>
2021-03-19 21:14:26 -0600

    Add Home tests

--- src/__tests__/Home.component.test.js
+++ src/__tests__/Home.component.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { mount, render, configure } from 'enzyme';
 import Home from '../pages/Home';
+import VideoCard from '../components/VideoCard';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
@@ -14,4 +15,9 @@ it('renders correctly', () => {
 it('check title by default', () => {
   const title = mount(<Home />);
   expect(title.find('h2').text()).toEqual('Mini Challenge 1');
+});
+
+it('renders VideoCard component', () => {
+  const wrapper = mount(<Home />);
+  expect(wrapper.containsMatchingElement(<VideoCard />)).toEqual(true);
 });
