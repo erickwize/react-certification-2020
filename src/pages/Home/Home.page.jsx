17114e3dabc1f7b18b40423bf625695ff4d4687c
Julieta <juliiy98@gmail.com>
2021-03-19 17:14:43 -0700

    deleted console warnings

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,21 +1,11 @@
 import React, { useRef } from 'react';
-import { Link, useHistory } from 'react-router-dom';
 
-import { useAuth } from '../../providers/Auth';
 import './Home.styles.css';
 
 import VideoPreviewCardList from '../../components/VideoPreview/VideoPreviewCardList';
 
 function HomePage() {
-  const history = useHistory();
   const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
-
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
-  }
 
   return (
     <section className="homepage" ref={sectionRef}>
