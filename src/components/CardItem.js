36da2f33ba1ea26de321799d1990299d2b8f5e77
Diego Limon <diegolimon@Diegos-MacBook-Air.local>
2021-03-19 21:07:46 -0600

    adding alt property to img in cardItem

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -3,7 +3,7 @@ import React from "react";
 function CardItem({ name, sprite, description }) {
   return (
     <div className="card-item">
-      <img src={sprite} />
+      <img src={sprite} alt=""/>
       <h2>{name}</h2>
       <h3>{description}</h3>
     </div>
