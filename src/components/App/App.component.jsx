407a433259c0002349af988f80a6292ce227023b
Edgar Barcenas <ejbarce@gmail.com>
2021-03-30 01:41:49 -0600

    fix import to build and release

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import HomePage from '../../pages/Home/Home.page';
-import Layout from '../Atoms/Layout';
+import Layout from '../Atoms/Layout/index';
 import Header from '../Organisms/Header/index';
 
 function App() {
