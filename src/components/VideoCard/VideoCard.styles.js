371d3550083d070c088bff43b3879d6f9d678939
AureAle <aperez33@ucol.mx>
2021-03-30 17:28:23 -0600

    Update styles

--- src/components/VideoCard/VideoCard.styles.js
+++ src/components/VideoCard/VideoCard.styles.js
@@ -8,8 +8,11 @@ export const Container = styled.div`
   opacity: 1;
   box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%),
     0px 1px 10px 0px rgb(0 0 0 / 12%);
-
   text-align: left;
+  padding-bottom: 20px;
+  :hover {
+    opacity: 0.5;
+  }
 `;
 
 export const Image = styled.div`
@@ -18,9 +21,6 @@ export const Image = styled.div`
   height: ${(props) => (props.height ? props.height : 180)}px;
   background-repeat: no-repeat;
   background-size: cover;
-  :hover {
-    opacity: 0.5;
-  }
 `;
 
 export const Title = styled.h3`
@@ -30,6 +30,7 @@ export const Title = styled.h3`
   font-weight: 500;
   margin: 0;
   padding: 10px;
+  line-height: 1.2em;
 `;
 
 export const Description = styled.p`
@@ -37,4 +38,12 @@ export const Description = styled.p`
   color: rgba(0, 0, 0, 0.54);
   text-align: left;
   padding: 10px;
+  margin: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  max-height: 119px;
+  -webkit-line-clamp: 6;
+  -webkit-box-orient: vertical;
+  padding-bottom: 30px;
 `;
