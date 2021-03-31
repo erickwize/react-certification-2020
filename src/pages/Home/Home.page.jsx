3b80278432cffa58bf1e997f496297396371a9cb
unknown <paola.elizal@gmail.com>
2021-03-30 19:20:48 -0600

    Fetch videos YT Api

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Layout from '../../components/Layout';
 import VideoCard from '../../components/VideoCard';
-import videos from '../../mockData/videos';
 
 const CardsContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
 `;
 
+const YOUTUBE_API = 'https://www.googleapis.com/youtube/v3/playlistItems';
+
+// export async function getServerSideProps() {
+//   const res = await fetch(
+//     `${YOUTUBE_API}?part=snippet&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+//   );
+//   const data = await res.json();
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
+
 function HomePage() {
+  const [youtubeVideo, setYoutubeVideo] = useState(null);
+  useEffect(() => {
+    async function getServerSideProps() {
+      const res = await fetch(
+        `${YOUTUBE_API}?part=snippet&playlistId=PLyzOXHX-1l7fbbYl3L0xPL482zoeLMhj2&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+      );
+      const data = await res.json();
+      setYoutubeVideo(data);
+      console.log(data);
+    }
+    getServerSideProps();
+  }, []);
+
   return (
     <Layout>
       <section>
         <CardsContainer>
-          {videos.items.map((video) => (
-            <VideoCard
-              key={video.etag}
-              title={video.snippet.title}
-              description={video.snippet.description}
-              thumbnail={video.snippet.thumbnails.default.url}
-            />
-          ))}
+          {youtubeVideo !== null &&
+            youtubeVideo.items.map((caja) => (
+              <VideoCard
+                key={caja.etag}
+                title={caja.snippet.title}
+                description={caja.snippet.description}
+                thumbnail={caja.snippet.thumbnails.default.url}
+              />
+            ))}
         </CardsContainer>
       </section>
     </Layout>
