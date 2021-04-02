90550838e5b6d76b42fdae7431288e1c0d473224
miriam-alvarezp <miriam.alvarezp@adevinta.com>
2021-04-02 16:29:34 -0600

    Mini-Challenge 3: Init Player page

---
+++ src/pages/VideoPlayer/VideoPlayer.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Player.styles.css';
+
+function VideoPlayer() {
+  return (
+    <section className="view-player">
+      Player view
+    </section>
+  );
+}
+
+export default VideoPlayer;
