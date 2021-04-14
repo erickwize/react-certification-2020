4d924daadcc647465f19e49944c937cccbb9c2fe
Elena Mena <51173079+emenaTechtonic@users.noreply.github.com>
2021-04-14 11:33:07 -0600

    Mini-Challenge 4: State Management

--- src/components/Menu/__tests__/Menu.test.js
+++ src/components/Menu/__tests__/Menu.test.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
 import renderer from 'react-test-renderer'
-import { fireEvent } from '@testing-library/svelte'
-import { render } from '@testing-library/react'
-import { Context, initialState } from '../../../context/context'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+
+import { Context } from '../../../context/context'
 import Menu from '../Menu'
 
-const state = {
-  target: 'Wizeline',
-  theme: false,
-  menu: false,
-  view: 'home',
-}
+const jsonFile = require('../../../utils/assets/youtube-videos-mock.json')
+
 describe('Testing Menu component', () => {
   test('Renders Menu without crashing', async () => {
+    const state = {
+      view: 'home',
+      data: jsonFile,
+    }
     const rendered = renderer.create(
       <Context.Provider value={{ state }}>
         <Menu />
@@ -21,18 +20,56 @@ describe('Testing Menu component', () => {
     )
     expect(rendered).toMatchSnapshot()
   })
-  test('Home view changed when button menu clicked', async () => {
+
+  test('Favorites view changed when button menu clicked', async () => {
+    const e = { target: { innerText: 'Favorites' } }
+
+    const state = {
+      view: 'home',
+      data: jsonFile,
+    }
+
+    const payload = { payload: 'favorites', type: 'SET_VIEW' }
+
+    const handleChange = jest.fn()
+    const dispatch = jest.fn()
+
     const { getByText } = render(
-      <Context.Provider value={{ state }}>
-        <Menu />
+      <Context.Provider value={{ state, dispatch }}>
+        <Menu handleChange={handleChange} />
       </Context.Provider>
     )
-    const linkHome = getByText('Home')
 
-    await fireEvent(
-      linkHome,
-      new MouseEvent('click', { type: 'SET_VIEW', payload: 'home' })
+    const container = await waitFor(() => getByText('Favorites'))
+    fireEvent.click(container, e)
+
+    expect(dispatch).toHaveBeenCalledTimes(3)
+    expect(dispatch).toHaveBeenCalledWith(payload)
+  })
+
+  test('Home view changed when button menu clicked', async () => {
+    const e = { target: { innerText: 'Home' } }
+
+    const state = {
+      view: 'favorite',
+      data: jsonFile,
+    }
+
+    const payload = { payload: 'home', type: 'SET_VIEW' }
+
+    const handleChange = jest.fn()
+    const dispatch = jest.fn()
+
+    const { getByText } = render(
+      <Context.Provider value={{ state, dispatch }}>
+        <Menu handleChange={handleChange} />
+      </Context.Provider>
     )
-    expect(initialState.view).toEqual('home')
+
+    const container = await waitFor(() => getByText('Home'))
+    fireEvent.click(container, e)
+
+    expect(dispatch).toHaveBeenCalledTimes(3)
+    expect(dispatch).toHaveBeenCalledWith(payload)
   })
 })
---
+++ src/context/reducer.js
@@ -0,0 +1,31 @@
+export const ContextReducer = (state, action) => {
+  switch (action.type) {
+    case 'TOGGLE_THEME':
+      return {
+        ...state,
+        theme: !state.theme,
+      }
+    case 'OPEN_MENU':
+      return {
+        ...state,
+        menu: action.payload,
+      }
+    case 'GET_DATA':
+      return {
+        ...state,
+        data: action.payload,
+      }
+    case 'SET_VIEW':
+      return {
+        ...state,
+        view: action.payload,
+      }
+    case 'SET_TARGET':
+      return {
+        ...state,
+        target: action.payload,
+      }
+    default:
+      return state
+  }
+}
